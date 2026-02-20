db4fef6756d3611626e9afed1d760bc120f8b541
kukustic <kust.07.04.05@gmail.com>
2026-02-20 17:42:51 +0300

    add: 4 lab

---
+++ main.js
@@ -0,0 +1,29 @@
+(function () {
+
+  window.addEventListener("load", function () {
+
+  const loadTime = performance.now();
+
+  const footer = document.querySelector(".site-footer");
+
+  if (footer) {
+    const info = document.createElement("p");
+    info.textContent = "Страница загружена за " + loadTime.toFixed(2) + " мс";
+    footer.appendChild(info);
+  }
+
+});
+
+  const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+  const links = document.querySelectorAll(".nav__link");
+
+  links.forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (linkPage === currentPage) {
+      link.classList.add("nav__link--active");
+    }
+  });
+
+})();
